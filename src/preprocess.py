1f1effbf2ce1d61d05a9b92dc635029ed2e49ae0
Mohit-Bhoir <bhoirmohit1010@gmail.com>
2026-03-08 20:30:21 +0000

    training pipeline updated

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,58 +6,36 @@ import datetime
 
 params = yaml.safe_load(open("params.yaml"))["preprocess"]
 
-def preprocess(input_path, X_train_path, X_test_path, y_train_path, y_test_path, test_days, n_lags):
+def preprocess(input_path, train_path, test_path, test_days):
     df = pd.read_csv(input_path, parse_dates=["time"], index_col="time")
 
-    df["returns"] = np.log(df["price"] / df["price"].shift(1))
-    df["direction"] = np.sign(df["returns"])
+    # df["returns"] = np.log(df["price"] / df["price"].shift(1))
+    # df["direction"] = np.sign(df["returns"])
 
-    cols = []
-    for lag in range(1, n_lags + 1):
-        column_name = f"returns_lag_{lag}"
-        df[column_name] = df["returns"].shift(lag)
-        cols.append(column_name)
-
-    df.dropna(inplace=True)
-
-    mean = df[cols].mean()
-    std = df[cols].std().replace(0, 1)
-    df.loc[:, cols] = (df[cols] - mean) / std
 
     test_start_date = df.index[-1] - datetime.timedelta(days=test_days)
 
+    
     train_data = df[df.index < test_start_date].copy()
     test_data = df[df.index >= test_start_date].copy()
-
-    X_train = train_data[cols]
-    y_train = train_data["direction"]
-    X_test = test_data[cols]
-    y_test = test_data["direction"]
 
     print(f"\nTraining samples: {len(train_data)}")
     print(f"Testing samples: {len(test_data)}")
     print(f"Training period: {train_data.index[0]} to {train_data.index[-1]}")
     print(f"Testing period: {test_data.index[0]} to {test_data.index[-1]}")
 
-    os.makedirs(os.path.dirname(X_train_path), exist_ok=True)
-    X_train.to_csv(X_train_path)
+    os.makedirs(os.path.dirname(train_path), exist_ok=True)
+    train_data.to_csv(train_path)
 
-    os.makedirs(os.path.dirname(X_test_path), exist_ok=True)
-    X_test.to_csv(X_test_path)
+    os.makedirs(os.path.dirname(test_path), exist_ok=True)
+    test_data.to_csv(test_path)
 
-    os.makedirs(os.path.dirname(y_train_path), exist_ok=True)
-    y_train.to_csv(y_train_path)
-
-    os.makedirs(os.path.dirname(y_test_path), exist_ok=True)
-    y_test.to_csv(y_test_path)
+    
 
 if __name__ == "__main__":
     preprocess(
         params["input_path"],
-        params["X_train_path"],
-        params["X_test_path"],
-        params["y_train_path"],
-        params["y_test_path"],
+        params["train_path"],
+        params["test_path"],
         params["test_days"],
-        params["n_lags"],
     )
--- src/train.py
+++ src/train.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import sys
-from tomlkit import datetime
 import yaml
 import os 
 import numpy as np
 import datetime
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix,accuracy_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
 import pickle
 from mlflow.models import infer_signature
 from urllib.parse import urlparse
 from pathlib import Path
 from dotenv import load_dotenv
 import mlflow
-from mlflow.models import infer_signature
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -34,10 +32,11 @@ os.environ["MLFLOW_TRACKING_PASSWORD"] = MLFLOW_TRACKING_PASSWORD
 
 def hyperparameter_tuning(X_train, y_train,param_grid,model_path):
     # Define the model
-    rf = RandomForestClassifier()
+    logistic_regression = LogisticRegression(random_state=42)
 
     # Perform grid search
-    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, n_jobs=-1)
+    tscv = TimeSeriesSplit(n_splits=5)
+    grid_search = GridSearchCV(estimator=logistic_regression, param_grid=param_grid, cv=tscv , n_jobs=-1)
     grid_search.fit(X_train, y_train)
 
     # Return the best hyperparameters
@@ -46,42 +45,50 @@ def hyperparameter_tuning(X_train, y_train,param_grid,model_path):
 #Load params from params.yaml
 params = yaml.safe_load(open("params.yaml"))['train']
 
-def train(X_train_path,y_train_path,X_test_path,y_test_path, model_path, param_grid):
-    X_train = pd.read_csv(X_train_path, index_col="time", parse_dates=["time"])
-    X_test = pd.read_csv(X_test_path, index_col="time", parse_dates=["time"])
+def train(train_path, model_path, param_grid, test_days, n_lags):
+    # Load the data
+    data = pd.read_csv(train_path, parse_dates=['time'], index_col='time')
 
-    y_train = pd.read_csv(y_train_path, index_col="time", parse_dates=["time"])["direction"]
-    y_test = pd.read_csv(y_test_path, index_col="time", parse_dates=["time"])["direction"]
+    #Generate columns / tech indicators here
+    data['returns'] = np.log(data['price'] / data['price'].shift(1))
+    data['direction'] = np.sign(data['returns'])
+    
+    #Split data with recent 7 days as test set here
+    test_start_date = data.index[-1] - datetime.timedelta(days=test_days)
+
+    cols = []
+    for lag in range(1, n_lags + 1):
+        data[f'returns_lag_{lag}'] = data['returns'].shift(lag)
+        cols.append(f'returns_lag_{lag}')
+    data.dropna(inplace=True)   
+    
+        
+    X = data[cols].copy()
+    y = data['direction']
+
+    X_train = X[X.index < test_start_date].copy()
+    y_train = y[y.index < test_start_date].copy()
+    X_test = X[X.index >= test_start_date].copy()
+    y_test = y[y.index >= test_start_date].copy()
     
     
-    signature = infer_signature(X_train,y_train)
     
-    # Perform hyperparameter tuning
-    grid_search = hyperparameter_tuning(X_train, y_train, param_grid, model_path)
-    best_model = grid_search.best_estimator_
-
-    # Train the model with the best hyperparameters
-    best_model.fit(X_train, y_train)
-
-    # Evaluate the model
-    y_pred = best_model.predict(X_test)
-    print("Classification Report:\n", classification_report(y_test, y_pred))
-    print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
-
+    signature = infer_signature(X_train, y_train)
+    
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
     #Start mlflow run 
-    mlflow.set_experiment("Random Forest Strategy for FX Trading")
+    mlflow.set_experiment("Logistic Regression Strategy for FX Trading")
     with mlflow.start_run():
         
         #Perform hyperparameter tuning again to log the best params
-        grid_search = hyperparameter_tuning(X_train, y_train, param_grid, model_path)
+        grid_search = hyperparameter_tuning(X_train[cols], y_train, param_grid, model_path)
         
         #Get the best model
         best_model = grid_search.best_estimator_
         
         #Predict and evaluate the model
-        y_pred = best_model.predict(X_test)
+        y_pred = best_model.predict(X_test[cols])
         accuracy = accuracy_score(y_test, y_pred)
         print(f"Accuracy: {accuracy}")
         
@@ -91,21 +98,25 @@ def train(X_train_path,y_train_path,X_test_path,y_test_path, model_path, param_g
         mlflow.log_params(grid_search.best_params_)
 
         tracking_url_type_store = urlparse(MLFLOW_TRACKING_URI).scheme
-        
-        if tracking_url_type_store != "file":
-            mlflow.sklearn.log_model(best_model, "model", registered_model_name="RandomForestFXTradingModel")
-        else:
-            mlflow.sklearn.log_model(best_model, "model", signature=signature)
-            
+
+        signature = infer_signature(X_train, best_model.predict(X_train))
+
+        mlflow.sklearn.log_model(
+        sk_model=best_model,
+        artifact_path="model",
+        signature=signature,
+        input_example=X_train.head(5),
+        registered_model_name="LogisticRegressionFXTradingModel"
+         )
         
         #Create the directory to save the model if it doesn't exist
         os.makedirs(os.path.dirname(model_path), exist_ok= True)
 
         filename = model_path
         pickle.dump(best_model,open(filename,'wb'))
-    
+        
         print(f"Model saved to {model_path}")
 
 
 if __name__ == "__main__":
-    train(params['X_train_path'],params['y_train_path'],params['X_test_path'],params['y_test_path'], params['model_path'], params['param_grid'])
+    train(params['train_path'], params['model_path'], params['param_grid'],params['test_days'], params['n_lags'])
